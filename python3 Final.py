ae2f37ba31540215c332cd09e2c5e4c26c3c96e8
L-met <103122953+L-met@users.noreply.github.com>
2022-04-08 13:21:16 -0400

    Update python3 Final.py

--- python3 Final.py
+++ python3 Final.py
@@ -137,11 +137,11 @@ app.layout = html.Div(children=[
 # Callback function definition
 # TASK4: Add 5 ouput components
 # Enter your code below. Make sure you have correct formatting.
-@app.callback(Output(component_id='plot1', component_property='children'),
+@app.callback([Output(component_id='plot1', component_property='children'),
             Output(component_id='plot2', component_property='children'),
             Output(component_id='plot3', component_property='children'),
             Output(component_id='plot4', component_property='children'),
-            Output(component_id='plot5', component_property='children'),
+            Output(component_id='plot5', component_property='children')],
             Input(component_id='input-type', component_property='value'),
             Input(component_id='input-year', component_property='value'), 
             State("plot1", 'children'), State("plot2", "children"), 
@@ -162,8 +162,7 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             
             # TASK5: Average flight time by reporting airline
             # Enter your code below. Make sure you have correct formatting.
-            line_fig = px.line(line_data, x='Month', y='Airtime', color='Reporting_Airline', title='Average monthly flight time (minutes) by airline')
-            
+            line_fig = px.line(line_data, x='Month', y='Airtime', color='Reporting_Airline', title='Average monthly flight time (minutes) by airline')  
             
             # Percentage of diverted airport landings per reporting airline
             pie_fig = px.pie(div_data, values='Flights', names='Reporting_Airline', title='% of flights by reporting airline')
